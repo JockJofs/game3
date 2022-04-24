62c53451a79d083333ff2ae88e9877d116855491
W <W@DESKTOP-M0C773V>
2022-04-24 13:15:14 +0300

    Добавьте файлы проекта.

---
+++ game3/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game3
+{
+    class tank
+    {// поля класса
+        public int HelPoint;
+        public int Armour;
+        public int Damaged;
+        public int Patron;
+        public string Name;
+        //конструктор класса
+        public tank(int a, int b, int c, int d, string f)
+        {
+            HelPoint = a; Armour = b; Damaged = c; Patron = d; Name = f;
+        }
+        public void Shot(tank enemy) //создаем метод выстрел
+        {
+            Random Shan = new Random(); //рандом
+            int Shos;
+
+            if (Patron <= 0)
+            {
+                Console.WriteLine("\nНеобходимо купить патроны\n");
+            }
+            else
+            {
+                Shos = Shan.Next(1, 11);
+                if (Shos <= 2)//добавление вероятности
+                {
+
+                    Console.WriteLine($"\n {Name} Промазал\n");
+                    Patron--;
+                }
+                else if (Shos == 3)
+                {
+                    double Crit = 1.2;
+                    enemy.HelPoint -= (int)(Damaged * Crit - enemy.Armour);
+                    Patron--;
+                    Console.WriteLine($"\n {Name} Наносит критическое попадание\n");
+                }
+                else
+                {
+                    enemy.HelPoint -= Damaged - enemy.Armour;
+                    Console.WriteLine($"{Name} Выстрелил");
+                    Patron--;
+                }
+            }
+        }
+        public void Heal()//метод починки
+        {
+            if (HelPoint >= 100)
+            {
+                Console.WriteLine("Починка не нужна");
+            }
+            else
+            {
+                HelPoint += 20;
+                Console.WriteLine("Произошла починка");
+
+                if (HelPoint >= 100)
+                {
+                    HelPoint = 100;
+                }
+
+
+            }
+        }
+
+        public void Buy()// метод покупки патронов
+        {
+            Patron = 3;
+        }
+    }
+}
---
+++ game3/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace game3
+{
+    class program
+
+    {
+        static void Main()
+        {
+
+            tank me = new(100, 15, 40, 3, "Igrok");//характеристики моего танка 
+            tank enemy = new(100, 15, 40, 3, "PC");//характеристики танка противника 
+            Random r = new Random();
+            int rand;
+            while (me.HelPoint > 0 && enemy.HelPoint > 0)
+            {
+
+                Console.WriteLine($"Количество жизней игрока : {me.HelPoint} Патроны : {me.Patron}");
+                Console.WriteLine($"Количество жизней противника : {enemy.HelPoint} Патроны :{enemy.Patron}\n");
+                Console.WriteLine("Выберите действие");
+                Console.WriteLine("Огонь :Z");
+                Console.WriteLine("Починка :X");
+                Console.WriteLine("Покупка патронов :C");
+
+                switch (Console.ReadKey().Key)//управление для игрока по клавишам
+                {
+                    case ConsoleKey.Z:
+                        me.Shot(enemy);
+                        break;
+                    case ConsoleKey.X:
+                        me.Heal();
+                        break;
+                    case ConsoleKey.C:
+                        me.Buy();
+                        break;
+                    default:
+                        Console.WriteLine("\nПропуск");
+                        break;
+                }
+
+                Console.WriteLine();
+                rand = r.Next(1, 3);
+
+                switch (rand)//компьютер работающий через рандом
+
+                {
+                    case 1:
+
+                        if (enemy.HelPoint > 50 && enemy.Patron > 0)
+                        { enemy.Shot(me); }
+                        else
+                        {
+                            if (enemy.HelPoint <= 50)
+                            {
+                                enemy.Heal();
+                            }
+                            else
+                            {
+                                enemy.Buy();
+                            }
+                        }
+                        break;
+                    case 2:
+
+                        if (enemy.Patron <= 0)
+                        {
+                            enemy.Buy();
+
+                        }
+                        else
+                        {
+                            enemy.Shot(me);
+
+                        }
+                        break;
+                }
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            if (me.HelPoint <= 0)//экран победы или поражения
+            {
+                Console.WriteLine("Поражение");
+            }
+            else
+            {
+                Console.WriteLine("Победа");
+            }
+
+        }
+    }
+}
